2c8ac92bc52f0aab206f6ed34749ade09f8836bd
gtuyub <gtuyub@dkt.com.mx>
2024-11-17 15:33:09 -0600

    refactoring changes on base subclasses to include new incremental id and new table names

--- src/models/orm_model.py
+++ src/models/orm_model.py
@@ -1,10 +1,12 @@
+from typing import Dict, List
+from sqlalchemy.orm import Session
 from .base import Base
 import sqlalchemy.types as types
 from sqlalchemy import Column
 from enum import Enum
 
 class CustomString(types.TypeDecorator):
-    """clase string personalizada para mapear 'empty strings' a NULL."""
+    """Custom string type decorator which maps empty strings to NULL"""
 
     impl = types.String
     cache_ok = True
@@ -21,115 +23,173 @@ class CustomString(types.TypeDecorator):
         return CustomString(self.impl.length)
 
 
-#subclases de la clase <Base>, representan las tablas de la base de datos.
-#Con el proposito de simplificar, el nombre de las clases coincide con el nombre del endpoint de la API de BC.
-#Asi mismo, los nombres de las columnas en cada clase, coinciden con los campos en la API de BC.
-#Por lo tanto, aqui se estan mapeando los campos de la API con los nombres de las columnas en SQL.
+#Subclasses of Base class, each represents a table on sql database.
+#Class names coincide with the corresponding api endpoint.
+#Therefore, we map api endpoint field names as class attributes, to sql table column names.
 
 class currencies(Base):
-    __tablename__ = 'Currency'
+    __tablename__ = 'currency'
 
-    code = Column('code',types.String,primary_key=True)
-    description = Column('name',CustomString)
+    code = Column('currency_code',types.String,unique=True)
+    description = Column('currency_name',CustomString)
 
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
+    
 class exchangeRates(Base):
-    __tablename__ = 'ExchangeRate'
+    __tablename__ = 'exchange_rate'
 
-    startingDate = Column('starting_date',types.Date,primary_key=True)
-    currencyCode = Column('currency_code',types.String,primary_key=True)
-    relationalCurrencyCode = Column('relational_currency_code',types.String,primary_key=True)
+    startingDate = Column('starting_date',types.Date)
+    currencyCode = Column('currency_code',types.String)
+    relationalCurrencyCode = Column('related_currency_code',types.String)
     exchangeRateAmount = Column('amount',types.Float)
 
-class paymentTerms(Base):
-    __tablename__ = 'PaymentTerms'
+    @classmethod
+    def get_update_keys(cls):
+        return [
+            'startingDate',
+            'currencyCode',
+            'relationalCurrencyCode'
+            ]
 
-    code = Column('code',types.String,primary_key=True)
-    description = Column('name',CustomString)
+class paymentTerms(Base):
+    __tablename__ = 'payment_terms'
+
+    code = Column('payment_terms_code',types.String,unique=True)
+    description = Column('payment_terms_name',CustomString)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
 
 class countries(Base):
-    __tablename__ = 'Country'
+    __tablename__ = 'country'
 
-    code = Column('code',types.String,primary_key=True)
-    name = Column('name',CustomString)
+    code = Column('country_code',types.String,unique=True)
+    name = Column('country_name',CustomString)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
 
 class shipmentMethods(Base):
-    __tablename__ = 'ShipmentMethod'
+    __tablename__ = 'shipment_method'
 
-    code = Column('code',types.String,primary_key=True)
-    description = Column('name',CustomString)
+    code = Column('shipment_method_code',types.String,unique=True)
+    description = Column('shipment_method_name',CustomString)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
 
 
 class priceGroups(Base):
-    __tablename__ = 'CustomerPriceGroup'
+    __tablename__ = 'customer_price_group'
 
-    code = Column('code',types.String,primary_key=True)
-    description = Column('name',CustomString)
+    code = Column('customer_price_group_code',types.String,unique=True)
+    description = Column('customer_price_group_name',CustomString)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
 
 class locations(Base):
-    __tablename__ = 'Location'
+    __tablename__ = 'location'
 
-    code = Column('code',types.String,primary_key=True)
-    name = Column('name',CustomString)
+    code = Column('location_code',types.String,unique=True)
+    name = Column('location_name',CustomString)
+    countryRegionCode = Column('country_code',CustomString)
+
+
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
 
 
 class paymentMethods(Base):
-    __tablename__ = 'PaymentMethod'
+    __tablename__ = 'payment_method'
 
-    code = Column('code',types.String,primary_key=True)
-    description = Column('name',CustomString)
+    code = Column('payment_method_code',types.String,unique=True)
+    description = Column('payment_method_name',CustomString)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
 
 
 class itemCategories(Base):
-    __tablename__ = 'ItemCategory'
+    __tablename__ = 'item_category'
 
-    code = Column('code',types.String,primary_key=True)
-    description = Column('name',CustomString)
+    code = Column('item_category_code',types.String,unique=True)
+    description = Column('item_category_name',CustomString)
     hasChildren = Column('has_children',types.Boolean)
     parentCategory = Column('parent_category',CustomString)
 
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
+
 
 class customerPostingGroups(Base):
-    __tablename__ = 'CustomerPostingGroup'
+    __tablename__ = 'customer_posting_group'
 
-    code = Column('code',types.String,primary_key=True)
-    description = Column('name',CustomString)
+    code = Column('customer_posting_group_code',types.String,unique=True)
+    description = Column('customer_posting_group_name',CustomString)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
 
 
 class vendorPostingGroups(Base):
-    __tablename__ = 'VendorPostingGroup'
+    __tablename__ = 'vendor_posting_group'
 
-    code = Column('code',types.String,primary_key=True)
-    description = Column('name',CustomString)
+    code = Column('vendor_posting_group_code',types.String,unique=True)
+    description = Column('vendor_posting_group_name',CustomString)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
 
 
 class inventoryPostingGroups(Base):
-    __tablename__ = 'InventoryPostingGroup'
+    __tablename__ = 'inventory_posting_group'
 
-    code = Column('code',types.String,primary_key=True)
-    description = Column('name',CustomString)
+    code = Column('inventory_posting_group_code',types.String,unique=True)
+    description = Column('inventory_posting_group_name',CustomString)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
 
 
 class customers(Base):
-    __tablename__ = 'Customer'
+    __tablename__ = 'customer'
 
-    no = Column('code',types.String,primary_key=True)
-    name = Column('name',CustomString)
+    no = Column('customer_code',types.String,unique=True)
+    name = Column('customer_name',CustomString)
     contact = Column('contact_code',CustomString)
-    customerPostingGroup = Column('customer_posting_group',CustomString)
-    customerPriceGroup = Column('customer_price_group',CustomString)
+    customerPostingGroup = Column('customer_posting_group_code',CustomString)
+    customerPriceGroup = Column('customer_price_group_code',CustomString)
     paymentTermsCode = Column('payment_terms_code',CustomString)
     countryRegionCode = Column('country_code',CustomString)
     locationCode = Column('location_code',CustomString)
     salespersonCode = Column('salesperson_code',CustomString)
     rfcNo = Column('rfc_code',CustomString)
-    blocked = Column('blocked',CustomString)
+    blocked = Column('is_blocked',CustomString)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['no']
 
 
 class vendors(Base):
-    __tablename__ = 'Vendor'
+    __tablename__ = 'vendor'
 
-    no = Column('code',types.String,primary_key=True)
-    name = Column('name',CustomString)
+    no = Column('vendor_code',types.String,unique=True)
+    name = Column('vendor_name',CustomString)
     primaryContactNo = Column('contact_code',CustomString)
     vendorPostingGroup = Column('vendor_posting_group',CustomString)
     paymentTermsCode = Column('payment_terms_code',CustomString)
@@ -137,38 +197,50 @@ class vendors(Base):
     locationCode = Column('location_code',CustomString)
     purchaserCode = Column('purchaser_code',CustomString)
     rfcNo = Column('rfc_code',CustomString)
-    blocked = Column('blocked',CustomString)
+    blocked = Column('is_blocked',CustomString)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['no']
 
 
 class salesmen(Base):
-    __tablename__ = 'Salesperson'
+    __tablename__ = 'salesperson'
 
-    code = Column('code',types.String,primary_key=True)
-    name = Column('name',CustomString)
-    blocked = Column('blocked',types.Boolean)
+    code = Column('salesperson_code',types.String,unique=True)
+    name = Column('salesperson_name',CustomString)
+    blocked = Column('is_blocked',types.Boolean)
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['code']
 
 
 class items(Base):
-    __tablename__ = 'Item'
+    __tablename__ = 'item'
 
-    no = Column('code',types.String,primary_key=True)
-    description = Column('name',CustomString)
+    no = Column('item_code',types.String,unique=True)
+    description = Column('item_name',CustomString)
     baseUnitOfMeasure = Column('measure_unit',CustomString)
     type = Column('item_type',CustomString)
-    inventoryPostingGroup = Column('inventory_posting_group',CustomString)
+    inventoryPostingGroup = Column('inventory_posting_group_code',CustomString)
     itemCategoryCode = Column('item_category_code',CustomString)
     unitPrice = Column('unit_price',types.Float)
     unitCost = Column('unit_cost',types.Float)
     grossWeight = Column('gross_weight',types.Float)
     netWeight = Column('net_weight',types.Float)
-    blocked = Column('blocked',types.Boolean)
+    blocked = Column('is_blocked',types.Boolean)
     salesBlocked = Column('sales_blocked',types.Boolean)
     purchasingBlocked = Column('purchase_blocked',types.Boolean)
 
-class customerLedgerEntries(Base):
-    __tablename__ = 'CustomerLedger'
+    @classmethod
+    def get_update_keys(cls):
+        return ['no']
 
-    entryNo = Column('entry_no',types.Integer,primary_key=True,autoincrement=False)
+class customerLedgerEntries(Base):
+    __tablename__ = 'customer_ledger'
+
+    entryNo = Column('entry_id',types.Integer,unique=True,autoincrement=False)
     postingDate = Column('posting_date',types.Date)
     documentDate = Column('document_date',types.Date)
     documentType = Column('document_type',CustomString)
@@ -180,18 +252,22 @@ class customerLedgerEntries(Base):
     positive = Column('is_positive',types.Boolean)
     transactionNo = Column('transaction_no',types.Integer)
     externalDocumentNo = Column('external_document_no',CustomString)
-    appliesToExtDocNo = Column('apply_to_external_document',CustomString)
+    appliesToExtDocNo = Column('apply_to_external_document_no',CustomString)
     closedByEntryNo = Column('closed_by_entry',types.Integer)
     open = Column('is_open',types.Boolean)
     reversed = Column('is_reversed',types.Boolean)
     reversedByEntryNo = Column('reversed_by_entry',types.Integer)
-    reversedEntryNo = Column('reversed_entry',types.Integer)    
+    reversedEntryNo = Column('reversed_entry',types.Integer)   
+
+    @classmethod
+    def get_update_keys(cls):
+        return ['entryNo'] 
 
 
 class vendorLedgerEntries(Base):
-    __tablename__ = 'VendorLedger'
+    __tablename__ = 'vendor_ledger'
 
-    entryNo = Column('entry_no',types.Integer,primary_key=True,autoincrement=False)
+    entryNo = Column('entry_id',types.Integer,unique=True,autoincrement=False)
     postingDate = Column('posting_date',types.Date)
     documentDate = Column('document_date',types.Date)
     documentType = Column('document_type',CustomString)
@@ -202,20 +278,24 @@ class vendorLedgerEntries(Base):
     remainingAmount = Column('remaining_amount',types.Float)
     positive = Column('is_positive',types.Boolean)
     transactionNo = Column('transaction_no',types.Integer)
-    externalDocumentNo = Column('external_document',CustomString)
-    appliesToExtDocNo = Column('apply_to_external_document',CustomString)
+    externalDocumentNo = Column('external_document_no',CustomString)
+    appliesToExtDocNo = Column('apply_to_external_document_no',CustomString)
     closedByEntryNo = Column('closed_by_entry',types.Integer)
     open = Column('is_open',types.Boolean)
     reversed = Column('is_reversed',types.Boolean)
     reversedByEntryNo = Column('reversed_by_entry')
     reversedEntryNo = Column('reversed_entry')    
 
+    @classmethod
+    def get_update_keys(cls):
+        return ['entryNo']
+
 
 class salesInvoices(Base):
-    __tablename__ = 'SalesInvoice'
+    __tablename__ = 'sales_invoice'
 
-    no = Column('document_no',types.String, primary_key=True)
-    custLedgerEntryNo = Column('entry_no',types.Integer)
+    no = Column('document_no',types.String, unique=True)
+    custLedgerEntryNo = Column('entry_id',types.Integer)
     postingDate = Column('posting_date', types.Date)
     documentDate = Column('document_date',types.Date)
     sellToCustomerNo = Column('customer_code',types.String)
@@ -229,12 +309,15 @@ class salesInvoices(Base):
     amountIncludingVAT = Column('amount_with_vat',types.Float)
     orderNo = Column('order_no',CustomString)
 
+    @classmethod
+    def get_update_keys(cls):
+        return ['no']
 
 class salesInvoiceLines(Base):
-    __tablename__ = 'SalesInvoiceLine'
+    __tablename__ = 'sales_invoice_line'
 
-    documentNo = Column('document_no',types.String,primary_key=True)
-    lineNo = Column('line_no',types.Integer,primary_key=True)
+    documentNo = Column('document_no',types.String)
+    lineNo = Column('line_no',types.Integer)
     type = Column('item_type',CustomString)
     no = Column('item_code',CustomString)
     quantity = Column('quantity',types.Integer)
@@ -244,12 +327,16 @@ class salesInvoiceLines(Base):
     amount = Column('amount',types.Float)
     amountIncludingVAT = Column('amount_with_vat',types.Float)
 
+    @classmethod
+    def get_update_keys(cls):
+        return ['documentNo','lineNo']
+
 
 class salesCreditMemos(Base):
-    __tablename__ = 'SalesCrMemo'
+    __tablename__ = 'sales_cr_memo'
 
-    no = Column('document_no',types.String, primary_key=True)
-    custLedgerEntryNo = Column('entry_no',types.Integer)
+    no = Column('document_no',types.String, unique=True)
+    custLedgerEntryNo = Column('entry_id',types.Integer)
     postingDate = Column('posting_date', types.Date)
     documentDate = Column('document_date',types.Date)
     sellToCustomerNo = Column('customer_code',CustomString)
@@ -263,12 +350,16 @@ class salesCreditMemos(Base):
     amountIncludingVAT = Column('amount_with_vat',types.Float)
     returnOrderNo = Column('return_order_no',CustomString)
 
+    @classmethod
+    def get_update_keys(cls):
+        return ['no']
+
     
 class salesCreditMemoLines(Base):
-    __tablename__ = 'SalesCrMemoLine'
+    __tablename__ = 'sales_cr_memo_line'
 
-    documentNo = Column('document_no',types.String,primary_key=True)
-    lineNo = Column('line_no',types.Integer,primary_key=True)
+    documentNo = Column('document_no',types.String)
+    lineNo = Column('line_no',types.Integer)
     type = Column('item_type',CustomString)
     no = Column('item_code',CustomString)
     quantity = Column('quantity',types.Integer)
@@ -278,12 +369,16 @@ class salesCreditMemoLines(Base):
     amount = Column('amount',types.Float)
     amountIncludingVAT = Column('amount_with_vat',types.Float)
 
+    @classmethod
+    def get_update_keys(cls):
+        return ['documentNo','lineNo']
+
 
 class purchaseInvoices(Base):
-    __tablename__ = 'purchaseInvoice'
+    __tablename__ = 'purchase_invoice'
 
-    no = Column('document_no',types.String, primary_key=True)
-    vendorLedgerEntryNo = Column('entry_no',types.Integer)
+    no = Column('document_no',types.String,unique=True)
+    vendorLedgerEntryNo = Column('entry_id',types.Integer)
     postingDate = Column('posting_date', types.Date)
     documentDate = Column('document_date',types.Date)
     buyFromVendorNo = Column('vendor_code',CustomString)
@@ -294,11 +389,15 @@ class purchaseInvoices(Base):
     amountIncludingVAT = Column('amount_with_vat',types.Float)
     orderNo = Column('order_no',CustomString)
 
-class purchaseCreditMemos(Base):
-    __tablename__ = 'PurchaseCrMemo'
+    @classmethod
+    def get_update_keys(cls):
+        return ['no']
 
-    no = Column('document_no',types.String, primary_key=True)
-    vendorLedgerEntryNo = Column('entry_no',types.Integer)
+class purchaseCreditMemos(Base):
+    __tablename__ = 'purchase_cr_memo'
+
+    no = Column('document_no',types.String, unique=True)
+    vendorLedgerEntryNo = Column('entry_id',types.Integer)
     postingDate = Column('posting_date', types.Date)
     documentDate = Column('document_date',types.Date)
     buyFromVendorNo = Column('vendor_code',CustomString)
@@ -309,11 +408,15 @@ class purchaseCreditMemos(Base):
     amountIncludingVAT = Column('amount_with_vat',types.Float)
     returnOrderNo = Column('return_order_no',CustomString)
 
-class purchaseInvoiceLines(Base):
-    __tablename__ = 'PurchaseInvoiceLine'
+    @classmethod
+    def get_update_keys(cls):
+        return ['no']
 
-    documentNo = Column('document_no',types.String,primary_key=True)
-    lineNo  = Column('line_no',types.Integer,primary_key=True)
+class purchaseInvoiceLines(Base):
+    __tablename__ = 'purchase_invoice_line'
+
+    documentNo = Column('document_no',types.String)
+    lineNo  = Column('line_no',types.Integer)
     type = Column('item_type', CustomString)
     no = Column('item_code',CustomString)
     quantity = Column('quantity',types.Integer)
@@ -323,11 +426,15 @@ class purchaseInvoiceLines(Base):
     amount = Column('amount',types.Float)
     amountIncludingVAT = Column('amount_with_vat',types.Float)
 
-class purchaseCreditMemoLines(Base):
-    __tablename__ = 'PurchaseCrMemoLine'
+    @classmethod
+    def get_update_keys(cls):
+        return ['documentNo', 'lineNo']
 
-    documentNo = Column('document_no',types.String,primary_key=True)
-    lineNo  = Column('line_no',types.Integer,primary_key=True)
+class purchaseCreditMemoLines(Base):
+    __tablename__ = 'purchase_cr_memo_line'
+
+    documentNo = Column('document_no',types.String)
+    lineNo  = Column('line_no',types.Integer)
     type = Column('item_type',CustomString)
     no = Column('item_code',CustomString)
     quantity = Column('quantity',types.Integer)
@@ -337,11 +444,12 @@ class purchaseCreditMemoLines(Base):
     amount = Column('amount',types.Float)
     amountIncludingVAT = Column('amount_with_vat',types.Float)
 
+    @classmethod
+    def get_update_keys(cls):
+        return ['documentNo', 'lineNo']
 
-class TablesEnum(str,Enum) :
-    """subclase de Enum que define una descripcion para cada modelo SQLALchemy.
-    Cuando se ejecuta un 'flow' o 'deployment' en modo personalizado en el servidor de Prefect, 
-    te permite escoger con un 'dropdown' que tablas actualizar, en base a los valores en este Enum."""
+
+class Tables(str,Enum) :
 
     currencies = 'Divisas'
     paymentTerms = 'TerminosPago'
